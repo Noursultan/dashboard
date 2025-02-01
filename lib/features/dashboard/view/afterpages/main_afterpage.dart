f1ce11c7ebf65a248b78cd03a9fa80b8d55cfb07
Ernazar <ernazaraibekov2017@gmail.com>
2025-02-01 19:10:20 +0600

    Create table

--- lib/features/dashboard/view/afterpages/main_afterpage.dart
+++ lib/features/dashboard/view/afterpages/main_afterpage.dart
@@ -45,211 +45,212 @@ class _MainAfterpageState extends State<MainAfterpage> {
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.only(
-          bottom: 137,
+          bottom: 20,
           top: 37,
           left: screenType.name == 'mobile' ? 16 : 50,
           right: screenType.name == 'mobile' ? 16 : 50),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'IzdeSim Analysis',
-                style: TextStyle(
-                    fontSize: screenType.name == 'mobile' ? 17 : 32,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'Баланс: ',
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'IzdeSim Analysis',
                   style: TextStyle(
-                    color: const Color.fromRGBO(128, 128, 128, 0.55),
-                    fontSize: screenType.name == 'mobile' ? 17 : 18.0,
-                    fontWeight: FontWeight.w400,
+                      fontSize: screenType.name == 'mobile' ? 17 : 32,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'Баланс: ',
+                    style: TextStyle(
+                      color: const Color.fromRGBO(128, 128, 128, 0.55),
+                      fontSize: screenType.name == 'mobile' ? 17 : 18.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: '10000\$',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: screenType.name == 'mobile' ? 17 : 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                  children: [
-                    TextSpan(
-                      text: '10000\$',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: screenType.name == 'mobile' ? 17 : 18.0,
-                        fontWeight: FontWeight.bold,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    child: SvgPicture.asset(
+                      analyticIcon(),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        type = DashboardType.analytics;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    child: SvgPicture.asset(
+                      colonIcon(),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        type = DashboardType.colon;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenType.name == 'mobile' ? 31 : 24,
+            ),
+            Row(
+              children: [
+                DatePickerView(
+                  title: 'Начало:',
+                  initialDate: startDate,
+                  lastDate: endDate,
+                  onDateChanged: (DateTime newDate) {
+                    setState(() {
+                      startDate = newDate;
+                    });
+                    print("Выбрана дата начала: $newDate");
+                  },
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                DatePickerView(
+                  title: 'Конец:',
+                  initialDate: endDate,
+                  firstDate: startDate,
+                  onDateChanged: (DateTime newDate) {
+                    setState(() {
+                      endDate = newDate;
+                    });
+                    print("Выбрана дата конца: $newDate");
+                  },
+                ),
+                Container(
+                  width: 1.5,
+                  height: 50,
+                  color: Colors.grey.shade300,
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                ),
+                Expanded(
+                  child: Scrollbar(
+                    thumbVisibility: true,
+                    trackVisibility: false,
+                    controller: _scrollController,
+                    thickness: 6,
+                    radius: Radius.circular(10),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      controller: _scrollController,
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Dropdown(
+                            title: 'Роль покупателя',
+                            items: const [
+                              'Все роли',
+                              'Businessman',
+                              'Employee',
+                              'Freelancer',
+                              'Retired'
+                            ],
+                            onDateChanged: (String value) {
+                              print(value);
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                          Dropdown(
+                            title: 'Агент',
+                            items: const [
+                              'Все агенты',
+                              'Businessman',
+                              'Employee',
+                              'Freelancer',
+                              'Retired'
+                            ],
+                            onDateChanged: (String value) {
+                              print(value);
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                          Dropdown(
+                            title: 'Страны',
+                            items: const [
+                              'Все страны',
+                              'Businessman',
+                              'Employee',
+                              'Freelancer',
+                              'Retired'
+                            ],
+                            onDateChanged: (String value) {
+                              print(value);
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                          Dropdown(
+                            title: 'Покрытие',
+                            items: const [
+                              'Все',
+                              'Businessman',
+                              'Employee',
+                              'Freelancer',
+                              'Retired'
+                            ],
+                            onDateChanged: (String value) {
+                              print(value);
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                          Dropdown(
+                            title: 'Пакеты',
+                            items: const [
+                              'Все',
+                              'Businessman',
+                              'Employee',
+                              'Freelancer',
+                              'Retired'
+                            ],
+                            onDateChanged: (String value) {
+                              print(value);
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: screenType.name == 'mobile' ? 16 : 82,
-          ),
-          Row(
-            children: [
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  child: SvgPicture.asset(
-                    analyticIcon(),
-                  ),
-                  onTap: () {
-                    setState(() {
-                      type = DashboardType.analytics;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  child: SvgPicture.asset(
-                    colonIcon(),
-                  ),
-                  onTap: () {
-                    setState(() {
-                      type = DashboardType.colon;
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: screenType.name == 'mobile' ? 31 : 24,
-          ),
-          Row(
-            children: [
-              DatePickerView(
-                title: 'Начало:',
-                initialDate: startDate,
-                lastDate: endDate,
-                onDateChanged: (DateTime newDate) {
-                  setState(() {
-                    startDate = newDate;
-                  });
-                  print("Выбрана дата начала: $newDate");
-                },
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              DatePickerView(
-                title: 'Конец:',
-                initialDate: endDate,
-                firstDate: startDate,
-                onDateChanged: (DateTime newDate) {
-                  setState(() {
-                    endDate = newDate;
-                  });
-                  print("Выбрана дата конца: $newDate");
-                },
-              ),
-              Container(
-                width: 1.5,
-                height: 50,
-                color: Colors.grey.shade300,
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-              ),
-              Expanded(
-                child: Scrollbar(
-                  thumbVisibility: true,
-                  trackVisibility: false,
-                  controller: _scrollController,
-                  thickness: 6,
-                  radius: Radius.circular(10),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    controller: _scrollController,
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Dropdown(
-                          title: 'Роль покупателя',
-                          items: const [
-                            'Все роли',
-                            'Businessman',
-                            'Employee',
-                            'Freelancer',
-                            'Retired'
-                          ],
-                          onDateChanged: (String value) {
-                            print(value);
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                        Dropdown(
-                          title: 'Агент',
-                          items: const [
-                            'Все агенты',
-                            'Businessman',
-                            'Employee',
-                            'Freelancer',
-                            'Retired'
-                          ],
-                          onDateChanged: (String value) {
-                            print(value);
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                        Dropdown(
-                          title: 'Страны',
-                          items: const [
-                            'Все страны',
-                            'Businessman',
-                            'Employee',
-                            'Freelancer',
-                            'Retired'
-                          ],
-                          onDateChanged: (String value) {
-                            print(value);
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                        Dropdown(
-                          title: 'Покрытие',
-                          items: const [
-                            'Все',
-                            'Businessman',
-                            'Employee',
-                            'Freelancer',
-                            'Retired'
-                          ],
-                          onDateChanged: (String value) {
-                            print(value);
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                        Dropdown(
-                          title: 'Пакеты',
-                          items: const [
-                            'Все',
-                            'Businessman',
-                            'Employee',
-                            'Freelancer',
-                            'Retired'
-                          ],
-                          onDateChanged: (String value) {
-                            print(value);
-                          },
-                        ),
-                      ],
-                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Row(
-            children: [DashboardTable()],
-          )
-        ],
+              ],
+            ),
+            const SizedBox(height: 8),
+            DashboardTable(),
+          ],
+        ),
       ),
     ));
   }
--- lib/features/dashboard/view/widgets/dashboard_table.dart
+++ lib/features/dashboard/view/widgets/dashboard_table.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 import 'widgets.dart';
 
+class OrderRow {
+  final String agentClient; // "Агент" / "Клиент"
+  final String userName; // "111@ww.ww"
+  final String eSimIccid; // "8985235052452007668"
+  final String airaloOrderId; // "20241110-336662"
+  final String coverageTitle; // "Spain"
+  final String packageId; // "guay-mobile-15days-2gb"
+  final double netPrice; // 2.7
+  final double agentPrice; // 6.5
+  final double netProfit; // 3.8
+
+  OrderRow({
+    required this.agentClient,
+    required this.userName,
+    required this.eSimIccid,
+    required this.airaloOrderId,
+    required this.coverageTitle,
+    required this.packageId,
+    required this.netPrice,
+    required this.agentPrice,
+    required this.netProfit,
+  });
+}
+
 class DashboardTable extends StatefulWidget {
   const DashboardTable({super.key});
 
@@ -9,6 +33,56 @@ class DashboardTable extends StatefulWidget {
 }
 
 class _DashboardTableState extends State<DashboardTable> {
+  final List<OrderRow> data = [
+    OrderRow(
+      agentClient: 'Агент',
+      userName: '111@ww.ww',
+      eSimIccid: '8985235052452007668',
+      airaloOrderId: '20241110-336662',
+      coverageTitle: 'Spain',
+      packageId: 'guay-mobile-15days-2gb',
+      netPrice: 2.7,
+      agentPrice: 6.5,
+      netProfit: 3.8,
+    ),
+    OrderRow(
+      agentClient: 'Агент',
+      userName: '111@ww.ww',
+      eSimIccid: '8985235052452007668',
+      airaloOrderId: '20241110-336662',
+      coverageTitle: 'Spain',
+      packageId: 'guay-mobile-15days-2gb',
+      netPrice: 2.7,
+      agentPrice: 6.5,
+      netProfit: 3.8,
+    ),
+    OrderRow(
+      agentClient: 'Агент',
+      userName: '111@ww.ww',
+      eSimIccid: '8985235052452007668',
+      airaloOrderId: '20241110-336662',
+      coverageTitle: 'Spain',
+      packageId: 'guay-mobile-15days-2gb',
+      netPrice: 2.7,
+      agentPrice: 6.5,
+      netProfit: 3.8,
+    ),
+    OrderRow(
+      agentClient: 'Клиент',
+      userName: '111@ww.ww',
+      eSimIccid: '8985235052452007668',
+      airaloOrderId: '20241110-336662',
+      coverageTitle: 'Spain',
+      packageId: 'guay-mobile-15days-2gb',
+      netPrice: 8.5,
+      agentPrice: 0,
+      // здесь 0, если у «клиента» нет наценки
+      netProfit: 8.5,
+    ),
+  ];
+
+  double get totalNetProfit => data.fold(0, (sum, row) => sum + row.netProfit);
+
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -31,6 +105,120 @@ class _DashboardTableState extends State<DashboardTable> {
             },
           ),
         ],
+      ),
+      const SizedBox(height: 24),
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          headingRowHeight: 100,
+            dataRowHeight: 56,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Color(0xFFE6E6E6)),
+            ),
+            columns: const [
+              DataColumn(label: Text(
+                'Идентификатор\nАгент/Клиент',
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black
+                )
+              )),
+              DataColumn(label: Text(
+                  'Имя пользователя',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Уникальный номер\nSIM карты ICCID',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Идентификатор заказа \nот Airalo \nOrder ID from Airalo',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Название покрытия \nCoverage Title',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Идентификатор\n пакета Package ID',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Чистая цена\nза единицу от \nАйралоNet Price',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Цена с \nдобавочной \nстоимостью для агента или \nклиента',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+              DataColumn(label: Text(
+                  'Чистая прибыль\nNet Profit',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                  )
+              )),
+            ],
+            rows: [
+              ...data.map((row) => DataRow(cells: [
+                    DataCell(Text(row.agentClient)),
+                    DataCell(Text(row.userName)),
+                    DataCell(Text(row.eSimIccid)),
+                    DataCell(Text(row.airaloOrderId)),
+                    DataCell(Text(row.coverageTitle)),
+                    DataCell(Text(row.packageId)),
+                    DataCell(Text(row.netPrice.toString())),
+                    DataCell(Text(row.agentPrice.toString())),
+                    DataCell(Text(row.netProfit.toString())),
+                  ])),
+              DataRow(
+                  color: MaterialStateProperty.all(Colors.grey.shade200),
+                  cells: [
+                    DataCell(Text('Итого\n(Total price)',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text('')),
+                    DataCell(Text(totalNetProfit.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                  ]),
+            ]),
       ),
     ]);
   }
